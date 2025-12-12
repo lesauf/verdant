d93e76e67cb740d0263773b8d6a2abebcb23ba5b
Stephane Fotso Teketchuet <sf@susoft.com>
2025-12-12 19:54:39 +0100

    Code cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,6 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'react-native';
-import { AppProviders } from '../contexts/AppProviders';
-import { BlocksProvider } from '../contexts/BlocksContext';
-import { TasksProvider } from '../contexts/TasksContext';
 import "../global.css";
 
 export const unstable_settings = {
@@ -14,28 +11,13 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
-  // Define all providers in an array
-  // Order matters: outer providers first, inner providers last
-  const providers = [
-    BlocksProvider,
-    TasksProvider,
-    // Future providers can be easily added here:
-    // InventoryProvider,
-    // GPSProvider,
-    // FinancesProvider,
-    // WorkersProvider,
-    // LivestockProvider,
-  ];
-
   return (
-    <AppProviders providers={providers}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          {/* <Stack.Screen name="+not-found" /> */}
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </AppProviders>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
